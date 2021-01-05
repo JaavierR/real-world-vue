456228a0d2d0ee7ecf867705dea09604bf5583fd
Javier Reyes <javier.reys@gmail.com>
2021-01-05 19:39:35 -0300

    feat: 3. state & getters.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,9 +1,38 @@
 <template>
   <div>
-    <h1>Create Event</h1>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>This is a test {{ localComputed }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  // ### 2 METHOD with extra computed properties ###
+  computed: {
+    localComputed() {
+      return this.user.name + ' with an extra computed property'
+    },
+    ...mapGetters(['catLength']),
+    // We use the spread operator
+    ...mapState(['user', 'categories'])
+  }
+
+  // ### 1 METHOD ###
+  // computed: mapState({
+  //   // userName: state => state.user.name,
+  //   // userID: state => state.user.id,
+  //   user: 'user',
+  //   // For top level states (i.e. not using dot notation), I could use a string instead.
+  //   categories: 'categories'
+  // })
+}
 </script>
+
+<style scoped></style>
