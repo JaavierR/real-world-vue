65124bde6729ae79c00b289ada3ce3f23d2f98e9
Javier Reyes <javier.reys@gmail.com>
2021-01-08 01:50:43 -0300

    feat: add Error pages with routes and handling errors

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Oopp!</h1>
+    <h3>The {{ resource }} you're looking is not here.</h3>
+    <router-link :to="{ name: 'event-list' }"
+      >Back to the Home Page</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped></style>
